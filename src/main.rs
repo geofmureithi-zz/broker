9395455cd3d8cdc8fdb0b6b60539ca5aa65c72d7
Bevan Hunt <bevan@bevanhunt.com>
2019-12-16 09:58:41 -0800

    fix counter to use new value

--- src/main.rs
+++ src/main.rs
@@ -9,29 +9,73 @@ pub struct MyData {
 }
 
 async fn download(data: web::Data<MyData>, broad: web::Data<std::sync::Mutex<Broadcaster>>) -> io::Result<NamedFile> {
+    
     let counter_buffer = data.db.get(b"counter").unwrap().unwrap();
     
     let counter = std::str::from_utf8(&counter_buffer).unwrap();
+    
     let counter_int = counter.clone().parse::<i32>().unwrap();
+    
     let new_counter_int = counter_int + 1;
+    
     let new_counter_string = new_counter_int.clone().to_string();
+    
     let new_counter = new_counter_string.as_bytes();
+    
     let old_counter = counter.clone().as_bytes();
 
     let _ = data.db.compare_and_swap(b"counter", Some(old_counter.clone()), Some(new_counter.clone()));
 
     let _ = web::block(move || data.db.flush()).await;
 
-    broadcast(counter.to_owned(), broad).await;
+    broadcast(new_counter_string.to_owned(), broad).await;
 
     let f = web::block(|| std::fs::File::create("test.pdf")).await.unwrap();
     
     NamedFile::from_file(f, "test.pdf")
 }
 
-async fn index() -> impl Responder {
+async fn index(data: web::Data<MyData>) -> impl Responder {
 
-    let content = include_str!("index.html");
+    // let content = include_str!("index.html");
+
+    let counter_buffer = data.db.get(b"counter").unwrap().unwrap();
+    
+    let counter = std::str::from_utf8(&counter_buffer).unwrap();
+
+    let a = r#"<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Server-sent events</title>
+        <style>
+            p {
+                margin-top: 0.5em;
+                margin-bottom: 0.5em;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="root"></div>
+        <script>
+            let root = document.getElementById("root");
+            let events = new EventSource("/events");
+            let data = document.createElement("p");
+            root.appendChild(data);
+            data.innerText = "#;
+        let b = format!("\"{}\";\n", counter);
+        let c = r#"
+            events.onmessage = (event) => {
+                if (event.data != "ping" && event.data != "connected") {
+                    data.innerText = event.data;
+                }
+            }
+        </script>
+    </body>
+    </html>"#;
+
+    let content = format!("{}{}{}", a, b, c);
 
     HttpResponse::Ok()
         .header("content-type", "text/html")
@@ -41,9 +85,7 @@ async fn index() -> impl Responder {
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     let tree = Db::open("./tmp/data").unwrap();
-    let counter_buffer = tree.get(b"counter").unwrap().unwrap();
-    let counter = std::str::from_utf8(&counter_buffer).unwrap().to_owned();
-    let data = Broadcaster::create(counter.to_string());
+    let data = Broadcaster::create("connected".to_owned());
 
     HttpServer::new(move || {
         App::new()
