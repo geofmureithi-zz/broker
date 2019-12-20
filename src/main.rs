6811a2e9fc8ec3d13c1adea7f06dca871dc8e8c9
Bevan Hunt <bevan@bevanhunt.com>
2019-12-20 14:25:51 +0000

    update example and benchmark

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use actix_web::web::Path;
 use actix_web::{web, HttpServer, HttpResponse, App, Responder, Error};
 use sse_actix_web::{new_client, Broadcaster, broadcast};
 use serde_derive::{Deserialize, Serialize};
@@ -7,9 +8,19 @@ struct MyUser {
     name: String,
 }
 
+async fn broad (
+    msg: Path<String>,
+    broadcaster: web::Data<std::sync::Mutex<Broadcaster>>,
+) -> impl Responder {
+    broadcaster.lock().unwrap().send("message", &msg.into_inner());
+
+    HttpResponse::Ok().body("msg sent")
+}
+
+
 async fn msg(item: web::Json<MyUser>, broad: web::Data<std::sync::Mutex<Broadcaster>>) -> Result<HttpResponse, Error> {
     let user_string = serde_json::to_string(&item.0).unwrap();
-    broadcast(user_string, broad).await;
+    broadcast("message".to_owned(), user_string, broad).await;
     Ok(HttpResponse::Ok().json(item.0))
 }
 
@@ -51,7 +62,7 @@ async fn index() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let data = Broadcaster::create("connected".to_owned());
+    let data = Broadcaster::create();
 
     HttpServer::new(move || {
         App::new()
@@ -59,6 +70,7 @@ async fn main() -> std::io::Result<()> {
             .data(web::JsonConfig::default())
             .route("/", web::get().to(index))
             .route("/msg", web::post().to(msg))
+            .route("/broadcast/{msg}", web::get().to(broad))
             .route("/events", web::get().to(new_client))
     })
     .bind("0.0.0.0:3000")?
