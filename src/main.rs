30e507e3d3f90d16f09e13aaab54981615e42ce7
Bevan Hunt <bevan@bevanhunt.com>
2019-12-30 15:34:11 -0800

    add websockets

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
+use actix::*;
 use actix_web::{http::header, web, HttpServer, HttpResponse, App, Error, Responder};
 use sse_actix_web::{Broadcaster, broadcast};
 use serde_derive::{Deserialize, Serialize};
 use std::sync::Mutex;
 use sled;
 use actix_cors::Cors;
+use ws_actix_web::{ws_index, WSClient, ClientCommand};
+use awc::{Client};
+use futures::stream::{StreamExt};
+use actix::io::SinkWrite;
 
 #[derive(Deserialize, Debug)]
 struct Config {
-  port: i32
+  port: String
 }
 
 pub struct MyData {
@@ -25,13 +30,35 @@ struct JSON {
     data: Data,
 }
 
-async fn new_client(data: web::Data<MyData>, broadcaster: web::Data<Mutex<Broadcaster>>) -> impl Responder {
+async fn new_client(data: web::Data<MyData>, broad: web::Data<Mutex<Broadcaster>>, config: web::Data<Config>) -> impl Responder {
 
     let user_buffer = data.db.get(b"user").unwrap().unwrap();
     
     let user = std::str::from_utf8(&user_buffer).unwrap();
 
-    let rx = broadcaster.lock().unwrap().new_client(&"user", user);
+    let rx = broad.lock().unwrap().new_client(&"user", user);
+
+    let ip2 = &config.port;
+
+    let (_response, framed) = Client::new()
+    .ws(ip2)
+    .connect()
+    .await
+    .map_err(|e| {
+        println!("Error: {}", e);
+    })
+    .unwrap();
+
+    let (sink, stream) = framed.split();
+    let addr = WSClient::create(|ctx| {
+        WSClient::add_stream(stream, ctx);
+        WSClient(SinkWrite::new(sink, ctx))
+    }); 
+
+    let cmd = String::new();
+    let cmd_clone = cmd.clone();
+    broadcast((&"user").to_string(), serde_json::from_str("{data: cmd}").unwrap(), broad.clone()).await;
+    addr.do_send(ClientCommand(cmd_clone));
 
     HttpResponse::Ok()
         .header("content-type", "text/event-stream")
@@ -64,6 +91,7 @@ async fn insert(data: web::Data<MyData>, broad: web::Data<Mutex<Broadcaster>>, j
 async fn main() -> std::io::Result<()> {
     let config = envy::from_env::<Config>().unwrap();
     let ip = format!("0.0.0.0:{}", config.port);
+    let ip2 = format!("0.0.0.0:{}/ws/", config.port);
   
     let tree = sled::open("./tmp/data").unwrap();
     let tree_clone = tree.clone();
@@ -83,10 +111,12 @@ async fn main() -> std::io::Result<()> {
                     .finish(),
             )
             .app_data(data.clone())
+            .data(Config{ port: ip2.clone()})
             .data(MyData{ db: tree_clone.clone()})
             .app_data(web::JsonConfig::default())
             .route("/insert", web::post().to(insert))
             .route("/events", web::get().to(new_client))
+            .service(web::resource("/ws/").route(web::get().to(ws_index)))
     })
     .bind(ip)?
     .run()
