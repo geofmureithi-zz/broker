cdbdf71c914cc54c5cad17aa33fd0dbfbd35b338
Bevan Hunt <bevan@bevanhunt.com>
2020-01-12 07:01:47 +0000

    test cancel

--- tests/tests.rs
+++ tests/tests.rs
@@ -4,6 +4,7 @@ use std::sync::mpsc;
 use std::{thread};
 use actix_rt::System;
 use actix_web::{dev::Server};
+use chrono::prelude::*;
 
 fn run_app(tx: mpsc::Sender<Server>, config: broker::Config) -> std::io::Result<()> {
     let mut sys = System::new("test");
@@ -32,7 +33,9 @@ fn full_test() {
     let user2 = json!({"username": "rust23", "password": "rust", "collection_id":"3ca76743-8d99-4d3f-b85c-633ea456f90d"});
     let user1_login = json!({"username": "rust22", "password": "rust"});
     let event1 = json!({"event": "test", "collection_id": "3ca76743-8d99-4d3f-b85c-633ea456f90c", "timestamp": 1578667309, "data": "{}"});
-    let event2 = json!({"event": "user", "collection_id": "3ca76743-8d99-4d3f-b85c-633ea456f90d", "timestamp": 1578667309, "data": "{}"});
+    let now = Utc::now().timestamp();
+    let x = now + 1000;
+    let event2 = json!({"event": "user", "collection_id": "3ca76743-8d99-4d3f-b85c-633ea456f90d", "timestamp": x, "data": "{}"});
 
     let client = reqwest::blocking::Client::new();
 
@@ -104,8 +107,8 @@ fn full_test() {
         .json(&event2)
         .send().unwrap();
     assert_eq!(res.status(), 200);
-    let event : broker::Record = serde_json::from_str(&res.text().unwrap()).unwrap();
-    assert_eq!(event.event.published, false);
+    let event2 : broker::Record = serde_json::from_str(&res.text().unwrap()).unwrap();
+    assert_eq!(event2.event.published, false);
 
     // try getting collection without auth - want failure
     let res = client.get("http://localhost:8000/events/collections/123")
@@ -147,10 +150,23 @@ fn full_test() {
 
     // try cancelling without auth - want failure
     let client = reqwest::blocking::Client::new();
-    let url = format!("http://localhost:8000/events/{}/cancel", event.event.id);
+    let url = format!("http://localhost:8000/events/{}/cancel", event2.event.id);
     let res = client.get(&url)
         .header("Authorization", &bearer)
-        .send().unwrap()
-        .status();
-    assert_eq!(res, 200);
+        .send().unwrap();
+    assert_eq!(res.status(), 200);
+    let event : broker::Record = serde_json::from_str(&res.text().unwrap()).unwrap();
+    assert_eq!(event.event.cancelled, true);
+
+    // pause for a second to process job
+    let one_second = std::time::Duration::from_millis(500);
+    std::thread::sleep(one_second);
+
+    // get collection - want success
+    let res = client.get("http://localhost:8000/events/collections/3ca76743-8d99-4d3f-b85c-633ea456f90d")
+        .header("Authorization", &bearer)
+        .send().unwrap();
+    assert_eq!(res.status(), 200);
+    let events : broker::Collection = serde_json::from_str(&res.text().unwrap()).unwrap();
+    assert_eq!(events.events[0].published, false);
 }
