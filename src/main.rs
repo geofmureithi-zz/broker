f7d32ae29e929fc0176cd5ca8ef32f2bd9724474
Bevan Hunt <bevan@bevanhunt.com>
2020-01-21 02:47:47 -0800

    start migration

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,192 @@
-mod lib;
-use lib::{broker_run};
+use std::collections::HashMap;
+use serde_derive::{Deserialize, Serialize};
+use serde_json::json;
+use uuid::Uuid;
+use bcrypt::{DEFAULT_COST, hash, verify};
+use chrono::prelude::*;
+use portal::{Filter, http::StatusCode};
+use jsonwebtoken::{encode, decode, Header, Validation};
 
-#[actix_rt::main]
-#[cfg_attr(tarpaulin, skip)]
-async fn main() -> std::result::Result<(), std::io::Error> {
-    broker_run().await
+#[derive(Deserialize, Debug, Clone)]
+pub struct Config {
+  pub port: String,
+  pub origin: String,
+  pub expiry: i64,
+  pub secret: String,
+  pub save_path: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct User {
+    id: uuid::Uuid,
+    username: String,
+    password: String,
+    collection_id: uuid::Uuid,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct UserForm {
+    username: String,
+    password: String,
+    collection_id: uuid::Uuid,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct Login {
+    username: String,
+    password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct Claims {
+    sub: String,
+    company: String,
+    exp: usize,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Cfg {
+  pub save_path: String,
+}
+
+fn user_create(tree: sled::Db, user_form: UserForm) -> (bool, String) {
+
+    // turn iVec(s) to String(s) and make HashMap
+    let records : HashMap<String, String> = tree.iter().into_iter().filter(|x| {
+        let p = x.as_ref().unwrap();
+        let k = std::str::from_utf8(&p.0).unwrap().to_owned();
+        let v = std::str::from_utf8(&p.1).unwrap().to_owned();
+        if k.contains("_u_") {
+            let user : User = serde_json::from_str(&v).unwrap();
+            if user.username == user_form.username {
+                return true
+            } else {
+                return false
+            }
+        }
+        return false
+    }).map(|x| {
+        let p = x.unwrap();
+        let k = std::str::from_utf8(&p.0).unwrap().to_owned();
+        let v = std::str::from_utf8(&p.1).unwrap().to_owned();
+        (k, v)
+    }).collect();
+
+    if records.len() > 0 {
+        let j = json!({"error": "username already taken"}).to_string();
+        return (false, j)
+    } else {
+        // set as future value
+        let uuid = Uuid::new_v4();
+        let versioned = format!("_u_{}", uuid.to_string());
+        let hashed = hash(user_form.clone().password, DEFAULT_COST).unwrap();
+        let new_user = User{id: uuid, username: user_form.clone().username, password: hashed, collection_id: user_form.collection_id };
+        
+        let _ = tree.compare_and_swap(versioned.as_bytes(), None as Option<&[u8]>, Some(serde_json::to_string(&new_user).unwrap().as_bytes())); 
+        let _ = tree.flush();
+        let j = json!({"id": uuid.to_string()}).to_string();
+        return (true, j)
+    }
+}
+
+fn login(tree: sled::Db, login: Login, config: Config) -> (bool, String) {
+    // add timestamp
+    let now = Utc::now().timestamp();
+    let expi = now + config.expiry;
+    let expiry = expi as usize;
+
+    // turn iVec(s) to String(s) and make HashMap
+    let records : HashMap<String, String> = tree.iter().into_iter().filter(|x| {
+        let p = x.as_ref().unwrap();
+        let k = std::str::from_utf8(&p.0).unwrap().to_owned();
+        if k.contains(&"_u_") {
+            let v = std::str::from_utf8(&p.1).unwrap().to_owned();
+            let user : User = serde_json::from_str(&v).unwrap();
+            if user.username == login.username {
+                return true
+            } else {
+                return false
+            }
+        } else {
+            return false
+        }
+    }).map(|x| {
+        let p = x.unwrap();
+        let k = std::str::from_utf8(&p.0).unwrap().to_owned();
+        let v = std::str::from_utf8(&p.1).unwrap().to_owned();
+        (k, v)
+    }).collect();
+
+    for (_k, v) in records {
+        let user : User = serde_json::from_str(&v).unwrap();
+        let verified = verify(login.password, &user.password).unwrap();
+        if verified {
+            let my_claims = Claims{company: "".to_owned(), sub: user.id.to_string(), exp: expiry};
+            let token = encode(&Header::default(), &my_claims, config.secret.as_ref()).unwrap();
+            let j = json!({"jwt": token}).to_string();
+            return (true, j)
+        } else {
+            return (false, "".to_owned())
+        }
+    }
+    (false, "".to_owned())
+}
+
+fn config() -> Config {
+    let mut port = "8080".to_owned();
+    let mut expiry : i64 = 3600;
+    let mut origin = "http://localhost:3000".to_owned();
+    let mut secret = "secret".to_owned();
+    let _ : Vec<String> = go_flag::parse(|flags| {
+        flags.add_flag("port", &mut port);
+        flags.add_flag("expiry", &mut expiry);
+        flags.add_flag("origin", &mut origin);
+        flags.add_flag("secret", &mut secret);
+    });
+
+    let cfg = envy::from_env::<Cfg>().unwrap();
+
+    Config{port: port, origin: origin, secret: secret, save_path: cfg.save_path, expiry: expiry}
+}
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    let user_create_route = portal::post()
+        .and(portal::path("users"))
+        .and(portal::body::json())
+        .map(|user: UserForm| {
+            let config = config();
+            let tree = sled::open(config.save_path).unwrap();
+            let (check, value) = user_create(tree.clone(), user.clone());
+            if check {
+                let reply = portal::reply::with_status(value, StatusCode::OK);
+                portal::reply::with_header(reply, "Content-Type", "application/json")
+            } else {
+                let reply = portal::reply::with_status(value, StatusCode::BAD_REQUEST);
+                portal::reply::with_header(reply, "Content-Type", "application/json")
+            }
+        });
+    
+    let login_route = portal::post()
+        .and(portal::path("login"))
+        .and(portal::body::json())
+        .map(|login_form: Login| {
+            let config = config();
+            let config_clone = config.clone();
+            let tree = sled::open(config.save_path).unwrap();
+            let (check, value) = login(tree.clone(), login_form.clone(), config_clone);
+            if check {
+                let reply = portal::reply::with_status(value, StatusCode::OK);
+                portal::reply::with_header(reply, "Content-Type", "application/json")
+            } else {
+                let reply = portal::reply::with_status(value, StatusCode::UNAUTHORIZED);
+                portal::reply::with_header(reply, "Content-Type", "application/json")
+            }
+        });
+
+    let routes = portal::any().and(user_create_route).or(login_route);
+
+    portal::serve(routes).run(([0, 0, 0, 0], 8080)).await
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,174 +1,174 @@
-extern crate broker;
-use serde_json::json;
-use std::sync::mpsc;
-use std::{thread};
-use actix_rt::System;
-use actix_web::{dev::Server};
-use chrono::prelude::*;
+// extern crate broker;
+// use serde_json::json;
+// use std::sync::mpsc;
+// use std::{thread};
+// use actix_rt::System;
+// use actix_web::{dev::Server};
+// use chrono::prelude::*;
 
-fn run_app(tx: mpsc::Sender<Server>, config: broker::Config) -> std::io::Result<()> {
-    let mut sys = System::new("test");
-    let srv = broker::server_create(config);
-    let _ = tx.send(srv.clone());
-    sys.block_on(srv)
-}
+// fn run_app(tx: mpsc::Sender<Server>, config: broker::Config) -> std::io::Result<()> {
+//     let mut sys = System::new("test");
+//     let srv = broker::server_create(config);
+//     let _ = tx.send(srv.clone());
+//     sys.block_on(srv)
+// }
 
-#[test]
-#[cfg_attr(tarpaulin, skip)]
-fn full_test() {
+// #[test]
+// #[cfg_attr(tarpaulin, skip)]
+// fn full_test() {
 
-    let (tx, rx) = mpsc::channel();
+//     let (tx, rx) = mpsc::channel();
 
-    thread::spawn(move || {
-        let id = uuid::Uuid::new_v4();
-        let p = format!("./tmp/{}", id.to_string());
-        let c = broker::Config{port: "8000".to_owned(), origin: "http://localhost:3000".to_owned(), expiry: 3600, secret: "secret".to_owned(), save_path: p};
+//     thread::spawn(move || {
+//         let id = uuid::Uuid::new_v4();
+//         let p = format!("./tmp/{}", id.to_string());
+//         let c = broker::Config{port: "8000".to_owned(), origin: "http://localhost:3000".to_owned(), expiry: 3600, secret: "secret".to_owned(), save_path: p};
 
-        let _ = run_app(tx, c);
+//         let _ = run_app(tx, c);
 
-        let second = std::time::Duration::from_millis(1000);
-        std::thread::sleep(second);
-    });
+//         let second = std::time::Duration::from_millis(1000);
+//         std::thread::sleep(second);
+//     });
 
-    let _ = rx.recv().unwrap();
+//     let _ = rx.recv().unwrap();
 
-    let second = std::time::Duration::from_millis(1000);
-    std::thread::sleep(second);
+//     let second = std::time::Duration::from_millis(1000);
+//     std::thread::sleep(second);
 
-    let user1 = json!({"username": "rust22", "password": "rust", "collection_id":"3ca76743-8d99-4d3f-b85c-633ea456f90c"});
-    let user2 = json!({"username": "rust23", "password": "rust", "collection_id":"3ca76743-8d99-4d3f-b85c-633ea456f90d"});
-    let user1_login = json!({"username": "rust22", "password": "rust"});
-    let event1 = json!({"event": "test", "collection_id": "3ca76743-8d99-4d3f-b85c-633ea456f90c", "timestamp": 1578667309, "data": "{}"});
-    let now = Utc::now().timestamp();
-    let x = now + 1000;
-    let event2 = json!({"event": "user", "collection_id": "3ca76743-8d99-4d3f-b85c-633ea456f90d", "timestamp": x, "data": "{}"});
+//     let user1 = json!({"username": "rust22", "password": "rust", "collection_id":"3ca76743-8d99-4d3f-b85c-633ea456f90c"});
+//     let user2 = json!({"username": "rust23", "password": "rust", "collection_id":"3ca76743-8d99-4d3f-b85c-633ea456f90d"});
+//     let user1_login = json!({"username": "rust22", "password": "rust"});
+//     let event1 = json!({"event": "test", "collection_id": "3ca76743-8d99-4d3f-b85c-633ea456f90c", "timestamp": 1578667309, "data": "{}"});
+//     let now = Utc::now().timestamp();
+//     let x = now + 1000;
+//     let event2 = json!({"event": "user", "collection_id": "3ca76743-8d99-4d3f-b85c-633ea456f90d", "timestamp": x, "data": "{}"});
 
-    let client = reqwest::blocking::Client::new();
+//     let client = reqwest::blocking::Client::new();
 
-    // create user 1 - want success
-    let res = client.post("http://localhost:8000/users")
-        .json(&user1)
-        .send().unwrap()
-        .status();
-    assert_eq!(res, 200);
+//     // create user 1 - want success
+//     let res = client.post("http://localhost:8000/users")
+//         .json(&user1)
+//         .send().unwrap()
+//         .status();
+//     assert_eq!(res, 200);
 
-    // create user 2 - want success
-    let res = client.post("http://localhost:8000/users")
-        .json(&user2)
-        .send().unwrap()
-        .status();
-    assert_eq!(res, 200);
+//     // create user 2 - want success
+//     let res = client.post("http://localhost:8000/users")
+//         .json(&user2)
+//         .send().unwrap()
+//         .status();
+//     assert_eq!(res, 200);
 
-    // try to create user 2 again - want failure
-    let res = client.post("http://localhost:8000/users")
-        .json(&user1)
-        .send().unwrap()
-        .status();
-    assert_eq!(res, 400);
+//     // try to create user 2 again - want failure
+//     let res = client.post("http://localhost:8000/users")
+//         .json(&user1)
+//         .send().unwrap()
+//         .status();
+//     assert_eq!(res, 400);
 
-    // login for user 1 - want success
-    let res = client.post("http://localhost:8000/login")
-        .json(&user1_login)
-        .send().unwrap()
-        .text().unwrap();
+//     // login for user 1 - want success
+//     let res = client.post("http://localhost:8000/login")
+//         .json(&user1_login)
+//         .send().unwrap()
+//         .text().unwrap();
     
-    let token: broker::Token = serde_json::from_str(&res).unwrap();
-    let bearer = format!("Bearer {}", token.jwt);
+//     let token: broker::Token = serde_json::from_str(&res).unwrap();
+//     let bearer = format!("Bearer {}", token.jwt);
 
-    // try posting event without auth - want failure
-    let res = client.post("http://localhost:8000/insert")
-        .json(&event1)
-        .send().unwrap()
-        .status();
-    assert_eq!(res, 401);
+//     // try posting event without auth - want failure
+//     let res = client.post("http://localhost:8000/insert")
+//         .json(&event1)
+//         .send().unwrap()
+//         .status();
+//     assert_eq!(res, 401);
 
-    // try posting event with bad auth - want failure
-    let res = client.post("http://localhost:8000/insert")
-        .header("Authorization", "foo")
-        .json(&event1)
-        .send().unwrap()
-        .status();
-    assert_eq!(res, 401);
+//     // try posting event with bad auth - want failure
+//     let res = client.post("http://localhost:8000/insert")
+//         .header("Authorization", "foo")
+//         .json(&event1)
+//         .send().unwrap()
+//         .status();
+//     assert_eq!(res, 401);
 
-    // try posting event with bad auth - want failure
-    let res = client.post("http://localhost:8000/insert")
-        .header("Authorization", "Bearer 1234")
-        .json(&event1)
-        .send().unwrap()
-        .status();
-    assert_eq!(res, 401);
+//     // try posting event with bad auth - want failure
+//     let res = client.post("http://localhost:8000/insert")
+//         .header("Authorization", "Bearer 1234")
+//         .json(&event1)
+//         .send().unwrap()
+//         .status();
+//     assert_eq!(res, 401);
 
-    // post event - want success
-    let res = client.post("http://localhost:8000/insert")
-        .header("Authorization", &bearer)
-        .json(&event1)
-        .send().unwrap();
-    assert_eq!(res.status(), 200);
-    let event : broker::Record = serde_json::from_str(&res.text().unwrap()).unwrap();
-    assert_eq!(event.event.published, false);
+//     // post event - want success
+//     let res = client.post("http://localhost:8000/insert")
+//         .header("Authorization", &bearer)
+//         .json(&event1)
+//         .send().unwrap();
+//     assert_eq!(res.status(), 200);
+//     let event : broker::Record = serde_json::from_str(&res.text().unwrap()).unwrap();
+//     assert_eq!(event.event.published, false);
 
-    // post event - want success
-    let res = client.post("http://localhost:8000/insert")
-        .header("Authorization", &bearer)
-        .json(&event2)
-        .send().unwrap();
-    assert_eq!(res.status(), 200);
-    let event2 : broker::Record = serde_json::from_str(&res.text().unwrap()).unwrap();
-    assert_eq!(event2.event.published, false);
+//     // post event - want success
+//     let res = client.post("http://localhost:8000/insert")
+//         .header("Authorization", &bearer)
+//         .json(&event2)
+//         .send().unwrap();
+//     assert_eq!(res.status(), 200);
+//     let event2 : broker::Record = serde_json::from_str(&res.text().unwrap()).unwrap();
+//     assert_eq!(event2.event.published, false);
 
-    // try getting collection without auth - want failure
-    let res = client.get("http://localhost:8000/events/collections/123")
-        .send().unwrap()
-        .status();
-    assert_eq!(res, 401);
+//     // try getting collection without auth - want failure
+//     let res = client.get("http://localhost:8000/events/collections/123")
+//         .send().unwrap()
+//         .status();
+//     assert_eq!(res, 401);
 
-    // pause for a second to process job
-    let half_second = std::time::Duration::from_millis(500);
-    std::thread::sleep(half_second);
+//     // pause for a second to process job
+//     let half_second = std::time::Duration::from_millis(500);
+//     std::thread::sleep(half_second);
 
-    // get collection - want success
-    let res = client.get("http://localhost:8000/events/collections/3ca76743-8d99-4d3f-b85c-633ea456f90c")
-        .header("Authorization", &bearer)
-        .send().unwrap();
-    assert_eq!(res.status(), 200);
-    let events : broker::Collection = serde_json::from_str(&res.text().unwrap()).unwrap();
-    assert_eq!(events.events[0].published, true);
+//     // get collection - want success
+//     let res = client.get("http://localhost:8000/events/collections/3ca76743-8d99-4d3f-b85c-633ea456f90c")
+//         .header("Authorization", &bearer)
+//         .send().unwrap();
+//     assert_eq!(res.status(), 200);
+//     let events : broker::Collection = serde_json::from_str(&res.text().unwrap()).unwrap();
+//     assert_eq!(events.events[0].published, true);
 
-    // try getting user without auth - want failure
-    let res = client.get("http://localhost:8000/events/user")
-        .send().unwrap()
-        .status();
-    assert_eq!(res, 401);
+//     // try getting user without auth - want failure
+//     let res = client.get("http://localhost:8000/events/user")
+//         .send().unwrap()
+//         .status();
+//     assert_eq!(res, 401);
 
-    // get user collection - want success
-    let res = client.get("http://localhost:8000/events/user")
-        .header("Authorization", &bearer)
-        .send().unwrap()
-        .status();
-    assert_eq!(res, 200);
+//     // get user collection - want success
+//     let res = client.get("http://localhost:8000/events/user")
+//         .header("Authorization", &bearer)
+//         .send().unwrap()
+//         .status();
+//     assert_eq!(res, 200);
 
-    // try cancelling without auth - want failure
-    let client = reqwest::blocking::Client::new();
-    let res = client.get("http://localhost:8000/events/123/cancel")
-        .send().unwrap()
-        .status();
-    assert_eq!(res, 401);
+//     // try cancelling without auth - want failure
+//     let client = reqwest::blocking::Client::new();
+//     let res = client.get("http://localhost:8000/events/123/cancel")
+//         .send().unwrap()
+//         .status();
+//     assert_eq!(res, 401);
 
-    // try cancelling without auth - want failure
-    let client = reqwest::blocking::Client::new();
-    let url = format!("http://localhost:8000/events/{}/cancel", event2.event.id);
-    let res = client.get(&url)
-        .header("Authorization", &bearer)
-        .send().unwrap();
-    assert_eq!(res.status(), 200);
-    let event : broker::Record = serde_json::from_str(&res.text().unwrap()).unwrap();
-    assert_eq!(event.event.cancelled, true);
+//     // try cancelling without auth - want failure
+//     let client = reqwest::blocking::Client::new();
+//     let url = format!("http://localhost:8000/events/{}/cancel", event2.event.id);
+//     let res = client.get(&url)
+//         .header("Authorization", &bearer)
+//         .send().unwrap();
+//     assert_eq!(res.status(), 200);
+//     let event : broker::Record = serde_json::from_str(&res.text().unwrap()).unwrap();
+//     assert_eq!(event.event.cancelled, true);
 
-    // get collection - want success
-    let res = client.get("http://localhost:8000/events/collections/3ca76743-8d99-4d3f-b85c-633ea456f90d")
-        .header("Authorization", &bearer)
-        .send().unwrap();
-    assert_eq!(res.status(), 200);
-    let events : broker::Collection = serde_json::from_str(&res.text().unwrap()).unwrap();
-    assert_eq!(events.events[0].published, false);
-}
+//     // get collection - want success
+//     let res = client.get("http://localhost:8000/events/collections/3ca76743-8d99-4d3f-b85c-633ea456f90d")
+//         .header("Authorization", &bearer)
+//         .send().unwrap();
+//     assert_eq!(res.status(), 200);
+//     let events : broker::Collection = serde_json::from_str(&res.text().unwrap()).unwrap();
+//     assert_eq!(events.events[0].published, false);
+// }
